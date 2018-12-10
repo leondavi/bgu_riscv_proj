b7d7ff743a06e2d5c6856ec165470ccb273dedf3
david <ddleon@outlook.co.il>
2018-12-10 18:27:42 +0200

    [MTHREADS_CPU] starting to work on it

---
+++ gem5/src/cpu/MthreadsCPU/cpu.hh
@@ -0,0 +1,14 @@
+#include "../minor/cpu.hh"
+#include "../minor/pipeline.hh"
+
+class Minor::Pipeline;
+
+class MThreadsPipeline : public Minor::Pipeline
+{
+
+};
+
+class MThreadsCPU : public MinorCPU
+{
+
+};
