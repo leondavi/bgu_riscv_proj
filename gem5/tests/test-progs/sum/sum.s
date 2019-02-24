6c21226ac1670c28afacfc56a6793f9ef097c5fb
Eni Yossi <eni.yossi@gmail.com>
2019-02-24 21:14:15 +0200

    compiled example gor gem5 riscv

---
+++ gem5/tests/test-progs/sum/sum.s
@@ -0,0 +1,34 @@
+	.file	"sum.cpp"
+	.option nopic
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	addi	sp,sp,-32
+	.cfi_def_cfa_offset 32
+	sd	s0,24(sp)
+	.cfi_offset 8, -8
+	addi	s0,sp,32
+	.cfi_def_cfa 8, 0
+	li	a5,5
+	sw	a5,-20(s0)
+	li	a5,7
+	sw	a5,-24(s0)
+	lw	a4,-20(s0)
+	lw	a5,-24(s0)
+	addw	a5,a4,a5
+	sw	a5,-28(s0)
+	li	a5,0
+	mv	a0,a5
+	ld	s0,24(sp)
+	.cfi_restore 8
+	addi	sp,sp,32
+	.cfi_def_cfa_register 2
+	jr	ra
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 8.2.0"
