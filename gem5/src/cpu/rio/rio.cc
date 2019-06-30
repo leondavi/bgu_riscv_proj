da96ba9f6f28a10591e06be698b28191e7c57349
Yossi Eni <yossie@example.com>
2019-06-30 16:04:28 +0300

    wakeup works and cleanup

--- gem5/src/cpu/rio/rio.cc
+++ gem5/src/cpu/rio/rio.cc
@@ -2,77 +2,76 @@
 #include "cpu/rio/pipeline.hh"
 #include "arch/utility.hh"
 
-RioCPU::RioCPU(RioCPUParams *params):
-BaseCPU(params),
-activityRecorder(name() + ".activity", 3, 3,0), // TODO - remove it to pipeline
-Icache(std::string("icache"),*this),
-Dcache(std::string("dcache"),*this)
+RioCPU::RioCPU(RioCPUParams *params) :
+		BaseCPU(params), activityRecorder(name() + ".activity", 3, 3, 0), // TODO - remove it to pipeline
+		Icache(std::string("icache"), *this), Dcache(std::string("dcache"),
+				*this)
 //pipeline()
 {
-    /* create threads */
-    Rio::RioThread *thread;
+	/* create threads */
+	Rio::RioThread *thread;
 
-    for (ThreadID i = 0; i < numThreads; i++)
-    {
-        if (FullSystem)
-        {
-            fatal("The Rio Model does not support FullSystem yet\n");
-        }
-        else
-        {
-            thread = new Rio::RioThread(this, i, params->system,
-                    params->workload[i], params->itb, params->dtb,
-                    params->isa[i]);
-        }
+	for (ThreadID i = 0; i < numThreads; i++) {
+		if (FullSystem) {
+			fatal("The Rio Model does not support FullSystem yet\n");
+		} else {
+			thread = new Rio::RioThread(this, i, params->system,
+					params->workload[i], params->itb, params->dtb,
+					params->isa[i]);
+		}
 
-        threads.push_back(thread);
-        ThreadContext *tc = thread->getTC();
-        threadContexts.push_back(tc);
-    }
+		threads.push_back(thread);
+		ThreadContext *tc = thread->getTC();
+		threadContexts.push_back(tc);
+	}
 
-    // TODO - chechk DynInst
-    //  Minor::MinorDynInst::init();
+	// TODO - chechk DynInst
+	//  Minor::MinorDynInst::init();
 
-};
+}
+;
 
-RioCPU *
-RioCPUParams::create()
-{
-    return new RioCPU(this);
+//=============================================================================
+RioCPU * RioCPUParams::create() {
+	return new RioCPU(this);
 }
 
-MasterPort &RioCPU::getInstPort()
-{
+//=============================================================================
+MasterPort &RioCPU::getInstPort() {
 //    return pipeline->getInstPort();
 	return Icache;
 }
 
-MasterPort &RioCPU::getDataPort()
-{
+//=============================================================================
+MasterPort &RioCPU::getDataPort() {
 //    return pipeline->getDataPort();
 	return Dcache;
 }
 
-Counter RioCPU::totalInsts() const
-{
-    Counter ret = 0;
+//=============================================================================
+Counter RioCPU::totalInsts() const {
+	Counter ret = 0;
+	for (auto i = threads.begin(); i != threads.end(); i++)
+		ret += (*i)->numInst;
 
-//    for (auto i = threads.begin(); i != threads.end(); i ++)
-//        ret += (*i)->numInst;
-
-    return ret;
+	return ret;
 }
 
-Counter RioCPU::totalOps() const
-{
-    Counter ret = 0;
-//
-//    for (auto i = threads.begin(); i != threads.end(); i ++)
-//        ret += (*i)->numOp;
+//=============================================================================
+Counter RioCPU::totalOps() const {
+	Counter ret = 0;
 
-    return ret;
+	for (auto i = threads.begin(); i != threads.end(); i++)
+		ret += (*i)->numOp;
+
+	return ret;
 }
 
-void RioCPU::wakeup(ThreadID tid){}
+//=============================================================================
+void RioCPU::wakeup(ThreadID tid) {
+	assert(tid < numThreads);
 
-
+	if (threads[tid]->status() == ThreadContext::Suspended) {
+		threads[tid]->activate();
+	}
+}
