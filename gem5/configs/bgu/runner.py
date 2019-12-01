f0cc5c519a3839c06fc5ebf8076fc8b2513faac6
Eni.Yossi <Eni.Yossi@gmail.com>
2019-12-01 20:23:16 +0200

    add some more params

--- gem5/configs/bgu/runner.py
+++ gem5/configs/bgu/runner.py
@@ -19,21 +19,11 @@ DEBUG_FLAG = "--debug-flags="
 RGR_WILDCARD = "BINARY_DIR"
 RGR_DIR_FLAG = " --outdir="   
 RGR_TRACKER = "job_tracker.txt"
-
 STATS_FILE = "stats.txt"
 #Parameters
 param_dict = dict()
-# TODO
-#    - parseResult(rgr)
-#    - genCSV()
-#    - runFromCSV()
-#    - cleanWorkdir(workdir)
-
-# progress
-# 1. fixed PARAM_STORE  path, add abspath
-# 2. add debug flag - to avoid execution
-# 3. generic functions: checkPointer,runCmd 
 runStatus=dict()
+
 ###############################################################################
 ###############################################################################
 ###############################################################################
@@ -191,6 +181,7 @@ def runRgrCmd(cmd,wd,config_file,rgr_file,rgr_wd,cpus,frame):
 
     pool = Pool(processes=int(cpus), maxtasksperchild=1)
     results = pool.map(runSingleCmd,cmd_list)
+    runPostProcessing(rgr_file,rgr_wd,[])
 
 # runPostProcessing()
 #==============================================================================
@@ -201,45 +192,57 @@ def runPostProcessing(rgr_file,rgr_wd,parse_list):
     if(permutation == None):
         return None
 
-    print "!"
     result_list=list()
     # 2. gen local cmd, modify output dir and append all parameters
-    for l in permutation:  
-        outdir = rgr_wd+"/"+genRgrDirName(l)
+    for perm in permutation:  
+        outdir = rgr_wd+"/"+genRgrDirName(perm)
         result = parseResult(outdir,parse_list)
-        result_list.append(l+result)
+        new_perm = list()
+        for p in perm:
+            new_perm.append(p.split("=")[1])
+        result_list.append(new_perm+result)
 
     key_list = key_list + ["status"]+parse_list
-    print_result(rgr_wd,header=key_list,data=result_list)
+    printCSV(rgr_wd,header=key_list,data=result_list)
 
     return 
-
-# runPostProcessing()
-#==============================================================================
-def print_result(rgr_wd,header,data):
-    result_file = rgr_wd+"/result.csv"
-    p_result_file = open(result_file,"w")
-    p_result_file.write("{0}\n".format(",".join(header)).replace("-",""))
-    for line in data:
-        p_result_file.write("{0}\n".format(",".join(line)))
-    p_result_file.close()
 
 # parseResult
 #==============================================================================
 def parseResult(outdir,parse_list):
     # chec that directory exists
-    print outdir
     if not checkPointer(outdir,"",isdir=True,verbose=False):
-        return ["NOT_RUN"]
+        return ["UNKNOWN"]
         
     p_file = outdir+"/"+STATS_FILE
     if os.stat(p_file).st_size ==0:
-        return ["Failed"]
+        return ["FAIL"]
 
-    result = ["PASSED"]
+    parse_result = [None]*len(parse_list)
+    p_read = open(p_file)
     
-    # TODO - loop over result
-    return result
+    for line in p_read:
+        # merege all space to 1 and split by one
+        data=(" ".join(line.split())).split (" ") 
+        if(len(data)==1):
+            continue
+        if data[0] in parse_list:
+            parse_result[parse_list.index(data[0])]=data[1]
+    p_read.close
+       
+    result = ["PASS"] + parse_result
+    return result 
+
+# printCSV
+#==============================================================================
+def printCSV(rgr_wd,header,data):
+    result_file = rgr_wd+"/result.csv"
+    p_result_file = open(result_file,"w")
+    p_result_file.write("{0}\n".format(",".join(header)).replace("-",""))
+    for line in data:                    
+        p_result_file.write("{0}\n".format(",".join(line)))
+    p_result_file.close()
+
     
 
 
@@ -265,6 +268,7 @@ def exeBuild(top_dict):
 
     # Run command
     runCmd(build_cmd,param_dict["gem5_dir"],top_dict["text"])
+    print "exeBuild pressed"
 
 
 # exeRun
@@ -288,6 +292,7 @@ def exeRun(top_dict):
    
     # Run command
     runCmd(build_cmd,param_dict["gem5_dir"],top_dict["text"])
+    print "exeRun pressed"
 
 # exeRegression
 #==============================================================================
@@ -319,13 +324,20 @@ def exeRegression(top_dict):
         config_file=param_dict["config_file"], rgr_file=param_dict["rgr_file"],
         rgr_wd=param_dict["rgr_wd"],cpus=param_dict["rgr_cpus"],
         frame=top_dict["text"])
+    print "exeRegression pressed"
 
 # exeRerun
 #==============================================================================
 def exeRerun(top_dict):
     getParamDict(top_dict) # update all parameters
+    if(not checkPointer(param_dict["rgr_wd"],"rgr_wd",True)):
+        return
+    result_file = param_dict["rgr_wd"]+"/result.csv"
+    if(not checkPointer(result_file,"rgr_wd",False)):
+        return 
+    # TODO - read CSV, key and valus, in key search t he location of status
+    # for any status that not PASS generate the full command for regresion
     print "exeRerun pressed"
-
 # exeParse
 #==============================================================================
 def exeParse(top_dict):
@@ -334,8 +346,9 @@ def exeParse(top_dict):
         return
     if(not checkPointer(param_dict["rgr_file"],"rgr_file",False)):
         return
+    parse_list = param_dict["post_parsing"].split(",")        
     runPostProcessing(rgr_file=param_dict["rgr_file"],
-        rgr_wd=param_dict["rgr_wd"],parse_list=[])# TODO
+        rgr_wd=param_dict["rgr_wd"],parse_list=parse_list)
     print "exeParse pressed"
 
 # exeClean
@@ -363,12 +376,21 @@ def exeHelp(top_dict):
         "       Each test result generated into rgr_wd directory, in case   \n"\
         "       rgr permutation exists, the test will be skipped.           \n"\
         "       The execution generate two file:                            \n"\
-        "         1. job tracker - all command that running                 \n"\
-        "         2. job status - generate report of test status            \n"\
+        "         1. tracker.txt - all command that run/skip-TBD            \n"\
+        "         2. result.csv - report of test each test status           \n"\
         "                         (optional: auto parse run[TBD])           \n"\
         "4.rerun\n"\
         "5.parse\n"\
-        "6.clean\n"
+        "  Requires: rgr_file,rgr_wd                                        \n"\
+        "  Optional: post_parsing                                           \n"\
+        "  Exe: Based om rgr file check in rgr_wd which test run and what is\n"\
+        "       there status:[UNKNOW,FAIL,PASS]                             \n"\
+        "       In additionin, extract post_parsing statisitcs from result. \n"\
+        "6.clean-TBD                                                        \n"\
+        "7.stop -TBD                                                      \n\n"\
+        "*sendMail-once execution done, send a mail-TBD                     \n"\
+        "*config_flags,post_parsing are lists, can insert multiplale        \n"\
+        " arguments using comma(,) as seperatore                            \n"
     frame.delete('1.0', END)
     frame.insert('end', s)
     frame.see(END)
@@ -382,9 +404,9 @@ def exeStop(top_dict):
 #==============================================================================
 def browseFunc(entry_set,name):
     if name == "gem5_dir" or name =="rgr_wd":
-        name = tkFileDialog.askdirectory() # TODO-change to support dir
+        name = tkFileDialog.askdirectory() 
     else:
-        name = tkFileDialog.askopenfilename() # TODO-change to support dir
+        name = tkFileDialog.askopenfilename() 
     entry_set.delete(0, 'end')
     entry_set.insert(0,name)
 
